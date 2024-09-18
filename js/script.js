05979e78952571d5b9361afc010bd07370d1d484
tarawebdv <taraabdolahzade@gmail.com>
2024-09-18 02:10:11 -0700

    prompt

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-var age = 23
-var MyName = "tara"
-var captiom = "Data types"
-var IsOnline = true
-alert(typeof IsOnline)
+/*
+var FName = prompt("What Is Your First Name?");
+alert(`welcome ${FName}`);
+*/
+var age = prompt('How old are you?', 23);
+alert(`You are ${age} years old!`); 
