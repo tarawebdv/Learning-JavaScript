264b23136165efe81ac065c585d900be55d02916
tarawebdv <taraabdolahzade@gmail.com>
2024-10-05 08:36:22 -0700

    if & else

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// if
 var num1 = 12
 var num2 = 5
 var num3 = 10
@@ -5,12 +6,11 @@ var num4 = 2
 
 var str1 = "5"
 var str2 = "6"
+ 
+if (num3 / num4 === num1) {
 
-// alert(num1==num2)  // 12==5 ? => false
-// alert(num4 + num3 == num1) // 2+10 == 12? => true
-
-// alert(num2 > num4) // 5>2  ? => true
-// alert (num1*num2 < num3) // 12*5 < 10 ? false
-
-// alert(str1 == num2) // "5"==5 ? => true
-alert(str1 === num2) // "5"===5 ? => false
+    alert( "yes, is equal")
+}
+ else {
+    alert("No Is Not Equal")
+ }
