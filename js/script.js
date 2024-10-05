859da5a88d6dfb5fe8856633ae162df59020dd9b
tarawebdv <taraabdolahzade@gmail.com>
2024-10-05 08:10:24 -0700

    Comparison operators

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-var str= "javascript"
-var str1 = "learning"
 var num1 = 12
 var num2 = 5
+var num3 = 10
+var num4 = 2
 
-// alert(str - num2)   // NAN =Not A Number
-// alert(typeof NaN)
-// alert("15" + 5)  //= NAN
-// alert("15" / 5)  //= 3
+var str1 = "5"
+var str2 = "6"
 
-// alert(isNaN(str/num2))  // true
-// alert(isNaN(str/str1))  // true
-alert(isNaN(num1/num2))  // false
+// alert(num1==num2)  // 12==5 ? => false
+// alert(num4 + num3 == num1) // 2+10 == 12? => true
+
+// alert(num2 > num4) // 5>2  ? => true
+// alert (num1*num2 < num3) // 12*5 < 10 ? false
+
+// alert(str1 == num2) // "5"==5 ? => true
+alert(str1 === num2) // "5"===5 ? => false
