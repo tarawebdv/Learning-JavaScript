d1e618c795add91c719eee048ad33e9188c221ee
tarawebdv <taraabdolahzade@gmail.com>
2024-10-07 08:14:36 -0700

    Bitwise Operator

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
+// Bitwise Operator
+
 var num1 = 15
-var num2 = 23
-var num3= 34
-if (num1*num2+num3 === 660) {
-    alert("it is 66" )
+var num2 = 5
+var num3= 3
+if (num3*num2 === 17 || num3*num1 ===65) {
+    alert("it is 65 or 17" )
     
-} else if(num1*num2+num3 < 85) {
-    alert( "it is < 85" )
+} else if(num2+num3 < 10 || num2*num3 === num1) {
+    alert( "it is 10 or 15" )
     
-} else if (num1*num2+num3 < 855) {
-    alert( "it is > 855" )
+} else if (num1+num3 === 8 || num2*num2 === num1) {
+    alert( "it is 8 or 25" )
 } else{
     alert("false")
 }
+
